9943af0c9c27f63b64a203199ed459d3922c7c84
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-01-19 18:41:12 -0500

    fix typing

--- src/mode/useKBarMode.ts
+++ src/mode/useKBarMode.ts
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import { useSyncExternalStore } from "use-sync-external-store/shim";
 import { Mode } from "./Mode";
 
-export function useKBarMode(mode: Mode) {
+export function useKBarMode<T extends object>(mode: Mode<T>) {
   const subscribe = useCallback((fn) => mode.subscribe(["change"], fn), [mode]);
   const currentMode = useSyncExternalStore(
     subscribe,
